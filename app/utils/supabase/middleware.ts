da4cad1af5e60512b04d6737af33d618e51a6e0c
Arnav Chauhan <arnav216@gmail.com>
2026-01-28 02:54:06 -0500

    fix url redirects after login

--- app/utils/supabase/middleware.ts
+++ app/utils/supabase/middleware.ts
@@ -52,7 +52,7 @@ export async function updateSession(request: NextRequest) {
     (request.nextUrl.pathname === '/login' || request.nextUrl.pathname === '/signup')
   ) {
     const url = request.nextUrl.clone()
-    url.pathname = '/'
+    url.pathname = '/dashboard'
     return NextResponse.redirect(url)
   }
 
